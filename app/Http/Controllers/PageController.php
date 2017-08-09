f83b73e76469446691bee4d1c48c96b8eac04b5c
hamdon <hamdongunner@gmail.com>
2017-08-09 19:56:01 +0300

    the get inserting whith the whereIn that work on array of ides or anything else

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -57,7 +57,7 @@ class PageController extends Controller
     {
 //        return DB::table('blogs')->select('id')->get(); // there is as that change the name just in the local host not ion the DB
 //        return DB::table('blogs')->select(DB::raw('count(*) as t_count'))->get();
-       $db = DB::table('blogs')->where('id','=',1)
+       $db = DB::table('blogs')->whereIn('id',[1,2])
            ->update(['title'=>"hello",'content'=>'faw','user_id'=>1]
         );
         if($db)
