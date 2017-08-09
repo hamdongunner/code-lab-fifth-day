3c95e5020c63f440d42b7cc3499a3421bfb72ebe
hamdon <hamdongunner@gmail.com>
2017-08-09 18:43:53 +0300

    the right way to the null title

--- database/migrations/2017_08_09_150032_cearte_blogs_table.php
+++ database/migrations/2017_08_09_150032_cearte_blogs_table.php
@@ -15,7 +15,7 @@ class CearteBlogsTable extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title')->default(null); // راح يعتبرها ك سترنك
+            $table->string('title')->nullable()->default(null);// هيجي صحيح راح تصير نل
             $table->longText('content')->nullable();
             $table->integer('user_id')->unsigned()->nullable();
             $table->timestamps();
